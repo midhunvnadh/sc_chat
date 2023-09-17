686f5c6e4eb5982eefb7457c1574e7c2d301fe58
midhunvnadh <midhun.vnadh5732@gmail.com>
2023-09-17 13:25:03 +0530

    Improve search of medicines

--- backend/get_best_medicines.py
+++ backend/get_best_medicines.py
@@ -9,7 +9,8 @@ def get_medicine_data():
     with open('medicine_data.json') as json_file:
         data = json.load(json_file)
         return data
-    
+
+
 def summarize_text(text, num_sentences=2):
     # Initialize the parser with the input text
     parser = PlaintextParser.from_string(text, Tokenizer("english"))
@@ -25,12 +26,13 @@ def summarize_text(text, num_sentences=2):
 
     return summarized_text
 
+
 def find_medicine(query, reason):
     query = summarize_text(query)
     print("Query: " + query)
     keywords = preprocess_text(query)
     data = get_medicine_data()
-    if(reason != None):
+    if (reason != None):
         reason = reason.lower()
         data = [medicine for medicine in data if reason in medicine["uses"].lower()]
     matches = []
@@ -44,10 +46,17 @@ def find_medicine(query, reason):
                 rank += 1
             if keyword in medicine["uses"]:
                 rank += 1
+            for tag in medicine["tags"]:
+                if keyword in tag:
+                    rank += 1
         if rank > 0:
             matches.append((rank, medicine))
     matches.sort(key=lambda tup: tup[0], reverse=True)
     matches = [match[1] for match in matches]
-    matches = [{"name" :match["name"], "description": match["description"], "uses": match["uses"]} for match in matches]
+    matches = [{"name": match["name"], "description": match["description"],
+                "uses": match["uses"]} for match in matches]
+
+    if (len(matches) == 0 and reason != None):
+        return find_medicine(query, None)
+
     return matches[:5]
- 
